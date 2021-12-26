fd2f2d3def29c2ef3eed844348c31711cfe20b46
Мусаев94 <beksultan1923@gmail.com>
2021-12-26 11:27:46 +0600

    Initial Commit

---
+++ InkyFlag1App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InkyFlag1App
+//
+//  Created by user on 26/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
